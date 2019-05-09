564b504e3cec096352052ddfa208f4079f05a76c
dusski <dusski@gmail.com>
2019-05-09 21:27:32 +0200

    Added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { VideoComponent } from './components/home/video/video.component';
+import { ChatComponent } from './components/home/chat/chat.component';
+import { ChatLogComponent } from './components/home/chat/chat-log/chat-log.component';
+import { ChatBoxComponent } from './components/home/chat/chat-box/chat-box.component';
+import { MessageComponent } from './components/home/chat/chat-log/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoComponent,
+    ChatComponent,
+    ChatLogComponent,
+    ChatBoxComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
