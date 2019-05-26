4bd907e01c4be7f5e92bdf552167165dd9f30991
dusski <dusski@gmail.com>
2019-05-26 22:33:44 +0200

    Added unload handler for page refresh/close Moved the logic for code parameter from AppC to HomeC Added room joining and leaving accordingly

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Router, ActivatedRoute, Params, RouterEvent, NavigationStart } from '@angular/router';
+import { ChatService } from 'src/app/services/chat.service';
+import { takeUntil, map, filter } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'shp-home',
@@ -8,11 +12,50 @@ import { Component, OnInit } from '@angular/core';
     class: "content-wrapper d-flex flex-grow-1 flex-md-row flex-column",
   }
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _roomCode: string;
+  private _destruction$: Subject<any> = new Subject<any>();
 
-  ngOnInit() {
+  @HostListener("window:beforeunload", ["$event"]) unloadHandler(event: Event) {
+    this._chatService.leaveRoom(this._roomCode);
   }
 
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _chatService: ChatService
+  ) { }
+
+  ngOnInit() {
+    this._route.paramMap.pipe(
+      takeUntil(this._destruction$),
+      map((params: Params) => params.get('code'))
+    ).subscribe(
+      (code: string) => {
+        console.log(code);
+        if (!code) {
+          code = this._chatService.generateRandomRoomCode()
+          this._router.navigate(['/', code])
+        }
+
+        this._roomCode = code
+        this._chatService.joinToRoom(code)
+
+        // this.chatService
+        //   .getMessages()
+        //   .subscribe((message: string) => {
+        //     this.messages = [...this.messages, message];
+        //   });
+
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this._destruction$.next();
+    this._destruction$.complete();
+  }
 }
